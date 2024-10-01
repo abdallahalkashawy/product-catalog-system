8661326bc9f7ec28940baf50a4f8d9949cad753f
Abdallah Ehab <abdallah.alkashawy@gmail.com>
2024-10-01 16:41:42 +0300

    Initialization

---
+++ src/main/java/org/backend/productcatalogsystem/ProductCatalogSystemApplication.java
@@ -0,0 +1,13 @@
+package org.backend.productcatalogsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/backend/productcatalogsystem/ProductCatalogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.productcatalogsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
